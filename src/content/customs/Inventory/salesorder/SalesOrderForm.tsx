be036eaaa03f5d015c06af01635c1389fb6d13a1
Ajmot Sk <aajmot@gmail.com>
2024-06-02 17:24:21 +0530

    salesorder page work

--- src/content/customs/Inventory/salesorder/SalesOrderForm.tsx
+++ src/content/customs/Inventory/salesorder/SalesOrderForm.tsx
@@ -1,17 +1,46 @@
 
-import { Box, Button, ButtonGroup, Card, CardContent, CardHeader, Container, Divider, Grid, MenuItem, TextField } from "@mui/material";
+import { Box, Button, ButtonGroup, Card, CardContent, CardHeader, Container, Divider, Grid, MenuItem, Table, TableBody, TableCell, TableContainer, TableFooter, TableHead, TableRow, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { RoleService } from "src/Admin/Services/RoleService";
+import { CustomerService } from "src/Inevntory/Services/CustomerService";
 import { SalesOrderService } from "src/Inevntory/Services/SalesOrderService";
+import Paper from '@mui/material/Paper';
+import { ProductService } from "src/Inevntory/Services/ProductService";
 
 export default function SalesOrderForm(props) {
     const [service, setservice] = useState(new SalesOrderService());
-    const [typeList, settypeList] = useState([]);
-    const [type, settype] = useState("Default");
+    const [customerService, setcustomerService] = useState(new CustomerService());
+    const [productService, setproductService] = useState(new ProductService());
+    const [customerList, setcustomerList] = useState([]);
+    const [customerId, setcustomerId] = useState<number>(0);
+    const [itemIndex, setitemIndex] = useState<number>(1);
+    const [productList, setproductList] = useState([]);
 
+    let [orderDetails, setorderDetails] = useState([
+        {
+            itemIndex: 1,
+            productId: 0,
+            unit: 0,
+            quantity: 0,
+            freeQuantity: 0,
+            mrp: 0,
+            rate: 0,
+            totalAmount: 0,
+            discountPercentage: 0,
+            discountAmount: 0,
+            schPercentage: 0,
+            schAmount: 0,
+            gstPercentage: 0,
+            gstAmount: 0,
+            grossAmount: 0,
+            roundOff: 0,
+            netAmount: 0
+        }
 
-    const handleTypeChange = (event) => {
-        settype(event.target.value);
+    ]);
+
+    const handleCustomerChange = (event) => {
+        setcustomerId(event.target.value);
     };
 
     const save = async (e: any) => {
@@ -40,13 +69,51 @@ export default function SalesOrderForm(props) {
         props?.setloader(false);
     }
 
-    const reset = () => {
-
+    const addItems = (e) => {
+        setitemIndex(itemIndex + 1);
+        e.preventDefault();
+        orderDetails.push(
+            {
+                itemIndex: itemIndex,
+                productId: 0,
+                unit: 0,
+                quantity: 0,
+                freeQuantity: 0,
+                mrp: 0,
+                rate: 0,
+                totalAmount: 0,
+                discountPercentage: 0,
+                discountAmount: 0,
+                schPercentage: 0,
+                schAmount: 0,
+                gstPercentage: 0,
+                gstAmount: 0,
+                grossAmount: 0,
+                roundOff: 0,
+                netAmount: 0
+            }
+        )
     }
 
+
     const getInitData = async () => {
-        //get  genders
-        settypeList(["Default", "Bronze", "Silver", "Gold", "Platinum"]);
+        //get customer
+        var customerResponse = await customerService.search({});
+        if (customerResponse && customerResponse.isSuccess) {
+            setcustomerList(customerResponse?.data);
+        }
+        else {
+            setcustomerList([]);
+        }
+
+        //get products 
+        const productResponse = await productService.search({});
+        if (productResponse && productResponse?.isSuccess) {
+            setproductList(productResponse?.data);
+        }
+        else {
+            setproductList([]);
+        }
     }
 
     useEffect(() => {
@@ -62,81 +129,197 @@ export default function SalesOrderForm(props) {
             autoComplete="off"
             onSubmit={save}
         >
-            {/* {typeList?.length > 0 && <TextField
+            {customerList?.length > 0 && <TextField
                 required
                 select
-                id="SalesOrderType"
-                label="Type"
-                value={type}
-                onChange={handleTypeChange}
+                id="customerId"
+                label="Customer"
+                value={customerId}
+                onChange={handleCustomerChange}
             >
-                {typeList.map((option, index) => (
-                    <MenuItem key={"type_" + index} value={option}>
-                        {option}
+                {customerList.map((option, index) => (
+                    <MenuItem key={"type_" + index} value={option.id}>
+                        {option.customerName}
                     </MenuItem>
                 ))}
             </TextField>
-            } */}
+            }
 
             <TextField
                 required
-                id="name"
-                label="Name"
-            />
-
-            <TextField
-                required
-                id="description"
-                label="Desc."
+                id="orderNumber"
+                label="Order#"
             />
 
             <TextField
                 required
-                id="hsn"
-                label="HSN"
+                id="orderDate"
+                label="Order Date."
+                type="date"
             />
 
-            <TextField
-                required
-                id="primaryUnit"
-                label="Primary Unit"
-            />
+            {/* <CardContent>
+                <TableContainer component={Paper}> */}
+            <Table sx={{ minWidth: 650, overflow: 'scroll' }} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="right">Unit</TableCell>
+                        <TableCell align="right">Qty</TableCell>
+                        <TableCell align="right">Free Qty</TableCell>
+                        <TableCell align="right">MRP</TableCell>
+                        <TableCell align="right">Rate</TableCell>
+                        <TableCell align="right">Total Amt</TableCell>
+                        <TableCell align="right">Discount(%)</TableCell>
+                        <TableCell align="right">Discount(Amt)</TableCell>
+                        <TableCell align="right">SCH(%)</TableCell>
+                        <TableCell align="right">SCH(Amt)</TableCell>
+                        <TableCell align="right">GST(%)</TableCell>
+                        <TableCell align="right">GST(Amt)</TableCell>
+                        <TableCell align="right">Gross(Amt)</TableCell>
+                        <TableCell align="right">Roundoff(Amt)</TableCell>
+                        <TableCell align="right">Net(Amt)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {orderDetails.map((row, index) => (
+                        <TableRow
+                            key={row?.itemIndex}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell align="right">
+                                {row?.itemIndex}
+                            </TableCell>
 
-            <TextField
-                required
-                id="secondaryUnit"
-                label="Secondary Unit"
-            />
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"productId" + index}
+                                    select
+                                    value={row?.productId}
+                                >
+                                    {productList.map((option, pindex) => (
+                                        <MenuItem key={"product_" + index + "_" + pindex} value={option?.id}>
+                                            {option?.name}
+                                        </MenuItem>
+                                    ))}
+                                </TextField>
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"unit" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"quantity" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"freeQuantity" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"mrp" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"rate" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"totalAmount" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"discountPercentage" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"discountAmount" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"schPercentage" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"schAmount" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"gstPercentage" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"gstAmount" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"grossAmount" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"roundOff" + index}
+                                />
+                            </TableCell>
+                            <TableCell align="right">
+                                <TextField
+                                    required
+                                    id={"netAmount" + index}
+                                />
+                            </TableCell>
 
 
-            <TextField
-                required
-                id="gstPercentage"
-                label="GST"
-                type="number"
-            />
 
-            <TextField
-                required
-                id="brandName"
-                label="Brand"
-            />
-            <TextField
-                required
-                id="manufacturer"
-                label="Manufacturer"
-            />
 
-            <TextField
-                required
-                id="composition"
-                label="Composition"
-            />
-            <TextField
-                required
-                id="tags"
-                label="tags"
-            />
+                        </TableRow>
+                    ))}
+                </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TableCell colSpan={16}>
+                            <Button sx={{ margin: 1 }}
+                                variant="contained"
+                                onClick={addItems}
+                            >
+                                Add
+                            </Button>
+                        </TableCell>
+                    </TableRow>
+
+                </TableFooter>
+            </Table>
+            {/*    </TableContainer>
+
+            </CardContent> */}
 
             <CardContent>
                 <Button sx={{ margin: 1 }}
